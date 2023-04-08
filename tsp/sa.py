ffbafd03eb8487ff2f52f6dadfddbd0b2da0019e
Francisco Muñoz <femunoz@dim.uchile.cl>
2023-04-08 11:54:24 -0400

    refactor, docs: Refactor to the class and documentation

--- tsp/sa.py
+++ tsp/sa.py
@@ -1,44 +1,26 @@
 from collections import deque
+from typing import Optional
 
 import numpy as np
-from numpy._typing import ArrayLike
 from numpy.random import Generator
 
-from tsp.kernels import KernelTSP, SwapKernelTSP
-from tsp.utils import _Route, _GeneratorLike
-
-_EPS = np.finfo("float64").eps
-
-
-# noinspection PyPep8Naming
-def default_cooling_schedule(k, T_0=25, k_0=1) -> float:
-    r"""
-    The default cooling schedule is
-
-    .. math::
-        T_k = (T_0 \cdot \ln(k_0) \ln(k + 1 + k_0))^{-1}
-
-    :param int k: The number of the iteration at the temperature :math:`T_k`.
-    :param float T_0: Initial temperature. Default is 25
-    :param float k_0: Iteration phase shift. Default is 1
-    :return: The temperature at iteration :math:`k`.
-    """
-    return (T_0 * np.log(k_0)) / (np.log(k + k_0))
+from tsp.kernels import SwapKernelTSP, KernelTSP
+from tsp.utils import _Route, _GeneratorLike, _EPS, exponential_cooling_schedule, _ArrayLike
 
 
 class SimulatedAnnealingTSP:
     """
     The Simulated Annealing algorithm for solving the Traveling Salesman Problem.
 
-    The novelty of this class, is that you can choose the kernel (which is equivalent to choosing the connections in the graph)
-    from the list of kernels in the module :py:module:`tsp.kernels`.
+    The novelty of this class, is that you can choose the kernel (which is equivalent to choosing the
+    connections in the graph) from the list of kernels in the module :py:mod:`tsp.kernels`.
     """
     def __init__(
             self,
-            dist_matrix: ArrayLike,
+            dist_matrix: _ArrayLike,
             n_iter: int,
-            cooling_schedule=default_cooling_schedule,
-            kernel: KernelTSP = None,
+            cooling_schedule=exponential_cooling_schedule,
+            kernel: Optional[KernelTSP] = None,
             seed: _GeneratorLike = None
     ):
         """
@@ -46,19 +28,24 @@ class SimulatedAnnealingTSP:
 
         :param dist_matrix: Matrix of distances. Must be a two-dimensional square matrix.
         :param n_iter: The number of iterations to run the algorithm.
-        :param cooling_schedule: Cooling schedule function. Must be a function that receives an integer :math:`k` and returns the temperature.
-        :param kernel: The kernel of the algorithm. This can be any of the :py:module:`tsp.kernels`. The default is :py:class:`SwapKernelTSP`.
+        :param cooling_schedule: Cooling schedule function. Must be a function that receives an integer
+        :math:`k` and returns the temperature.
+        :param kernel: The kernel of the algorithm. This can be any of the :py:mod:`tsp.kernels`.
+        The default is :py:class:`SwapKernelTSP`.
         :param seed: Seed or generator. The kernel generator will be used if this parameter is None.
         """
         # Define the distance matrix and some asserts.
         self.dist_matrix: np.ndarray = np.asarray(dist_matrix)
         if len(self.dist_matrix.shape) != 2:
-            raise ValueError(f"The distance matrix must have dimension 2, not {len(self.dist_matrix.shape)}.")
+            raise ValueError("The distance matrix must have dimension 2,"
+                             f" not {len(self.dist_matrix.shape)}.")
         n, m = self.dist_matrix.shape
         if n != m:
             raise ValueError(f"The distance matrix must be square, not {n, m}.")
+
         # Normalise the distance matrix for stability
         self.dist_matrix /= np.max(self.dist_matrix)
+
         # Define the length of the route.
         self.len_route = n
 
@@ -74,39 +61,50 @@ class SimulatedAnnealingTSP:
             self.rng = self.kernel.rng
 
         # For statistics
-        self._accept_prob: list[float] = []
+        self._log_accept_prob: list[float] = []
+        self._n_accept: int = 0
+        self._acception_ratio: list[float] = []
         self._values: list[float] = []
         self._best_route: dict[str, float | _Route | None] = {
-            "value": None,
+            "value": float("inf"),
             "route": None,
         }
+        self._last_route = None
+        self._last_k = 0
 
     @property
-    def accept_prob(self):
-        """The acceptance probability history."""
-        return np.array(self._accept_prob)
-
-    @property
-    def values(self):
+    def values(self) -> np.ndarray:
         """The values of the total distances of the routes."""
         return np.array(self._values)
 
     @property
-    def abs_min_values(self):
+    def best_route(self) -> _Route:
+        """The best route found by the algorithm."""
+        return self._best_route["route"]
+
+    @property
+    def best_value(self) -> float:
+        """The best value found by the algorithm."""
+        return self._best_route["value"]
+
+    @property
+    def accept_prob(self) -> np.ndarray:
+        """The acceptance probability history."""
+        return np.exp(np.array(self._log_accept_prob))
+
+    @property
+    def abs_min_values(self) -> np.ndarray:
         """The absolute minimum value."""
         return np.minimum.accumulate(self.values)
 
-    @property
-    def best_route(self):
-        """The best route found by the algorithm."""
-        return np.array(self._best_route["route"])
-
     def dist_route(self, route: _Route) -> float:
-        r"""Compute the total distance the traveller should travel when executing the route.
-        If the route is of the form :math:`x = (x_0, \ldots, x_n)`, then the total distance is computed by means of
+        r"""
+        Compute the total distance the traveller should travel when executing the route.
+        If the route is of the form :math:`x = (x_0, \ldots, x_n)`, then the total distance is computed
+        by
 
         .. math::
-            \sum_{k = 0}^{n} d(x_k, x_{k+1})
+            \sum_{k=0}^{n} d(x_k, x_{k+1})
 
         With the convention that :math:`x_{n+1} = x_0`.
 
@@ -119,47 +117,49 @@ class SimulatedAnnealingTSP:
         return total_dist
 
     def _init_route(self):
-        """Compute a greedy route to facilitate optimisation."""
-        # Start with the first city
-        route_to_return = [0]
-        # Set of next candidates
-        candidates = {i for i in range(1, self.len_route)}
-        # While there are candidates...
-        while candidates:
-            last_city = route_to_return[-1]
-            # Obtain the distance between the last city and the candidates.
-            dist_candidates = {k: self.dist_matrix[last_city, k] for k in candidates}
-            # Get the candidate city with less distance
-            argmin_candidate = min(dist_candidates, key=dist_candidates.get)
-            route_to_return.append(argmin_candidate)
-            candidates.remove(argmin_candidate)
+        """Compute the initial route."""
+        if self._last_route is not None:
+            return self._last_route
+
+        route_to_return = list(self.rng.choice(self.len_route, size=self.len_route, replace=False))
 
         return route_to_return
+
+    def _register_route(self, route: _Route):
+        """Records route properties for later statistical purposes."""
+        dist_route = self.dist_route(route)
+        self._values.append(dist_route)
+        return route, dist_route
+
+    def _update_best_route(self, route: _Route, dist_route: float):
+        """Updates the best route, in case there was one."""
+        if dist_route < self._best_route["value"]:
+            self._best_route.update(value=dist_route, route=route)
 
     def run(self):
         """Execute the algorithm."""
         # Initial route
-        route = self._init_route()
-        dist_route = self.dist_route(route)
-        self._values.append(dist_route)
-        self._best_route.update(value=dist_route, route=route)
+        route, dist_route = self._register_route(self._init_route())
+        self._update_best_route(route, dist_route)
 
-        for k in range(self.n_iter):
+        for k in range(self._last_k, self.n_iter + self._last_k):
             # Sample a new route from the kernel
-            new_route = self.kernel.sample(route)
-            dist_new_route = self.dist_route(new_route)
-            self._values.append(dist_new_route)
+            new_route, dist_new_route = self._register_route(self.kernel(route))
 
             # Compute the acceptance probability
             gap = dist_new_route - dist_route
-            accept_prob = np.exp(0 if gap <= 0 else -gap / (self.temp(k) + _EPS))
-            self._accept_prob.append(accept_prob)
+            log_accept_prob = 0 if gap <= 0 else -gap / (self.temp(k) + _EPS)
+            self._log_accept_prob.append(log_accept_prob)
 
-            if dist_new_route <= 0 or self.rng.uniform() <= accept_prob:
+            if log_accept_prob >= 0 or self.rng.uniform() <= np.exp(log_accept_prob):
                 # Update the best route
-                if self._best_route["value"] >= dist_new_route:
-                    self._best_route.update(value=dist_new_route, route=new_route)
+                self._update_best_route(route, dist_new_route)
+                self._n_accept += 1
 
                 # Set the new route as the route
                 route, dist_route = new_route, dist_new_route
 
+            self._acception_ratio.append(self._n_accept / (k + 1))
+            self._last_k = k + 1
+
+        self._last_route = route
--- tsp/utils.py
+++ tsp/utils.py
@@ -1,10 +1,12 @@
 import functools
+from typing import Iterable
 
 import numpy as np
 from numpy.random import Generator
 
 _Route = list[int]
 _GeneratorLike = None | int | Generator
+_ArrayLike = np.ndarray | Iterable
 
 _EPS = np.finfo(np.float64).eps
 
@@ -27,3 +29,35 @@ def repeat(times: int):
         return wrapper
 
     return decorator_repeat
+
+
+# noinspection PyPep8Naming
+def logarithmic_cooling_schedule(k, T_0=25, k_0=1) -> float:
+    r"""
+    The logarithmic cooling schedule is
+
+    .. math::
+        T_k = T_0 \cdot \ln(k_0 + 1) / \ln(k + 1 + k_0)
+
+    :param int k: The number of the iteration at the temperature :math:`T_k`.
+    :param float T_0: Initial temperature. Default is 25
+    :param float k_0: Iteration phase shift. Default is 1
+    :return: The temperature at iteration :math:`k`.
+    """
+    return (T_0 * np.log(k_0)) / (np.log(k + k_0))
+
+
+# noinspection PyPep8Naming
+def exponential_cooling_schedule(k, T_0=100, rho=0.99) -> float:
+    r"""
+    The exponential cooling schedule is
+
+    .. math::
+        T_k = \rho^k \cdot T_0
+
+    :param int k: The number of the iteration at the temperature :math:`T_k`.
+    :param float T_0: Initial temperature. Default is 100.
+    :param rho: The decay parameter. The default is 0.99. It is recommended to increase this amount to a value of 0.999 or 0.9999...
+    :return: The temperature at iteration :math:`k`.
+    """
+    return rho ** k * T_0
