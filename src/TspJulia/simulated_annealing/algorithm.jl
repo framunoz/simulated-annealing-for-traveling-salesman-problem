8afccedf8ec775fe9f7433fff116c84ccacbec02
Francisco Muñoz <femunoz@dim.uchile.cl>
2026-01-18 23:26:52 -0300

    debug: fix sa algorithm and improve the function design

--- src/TspJulia/simulated_annealing/algorithm.jl
+++ src/TspJulia/simulated_annealing/algorithm.jl
@@ -1,52 +1,72 @@
-initial_route(sa::AbstractSimulatedAnnealing)::Elements.Route =
-    Elements.Route(1:length(get_cities(sa)))
+function initial_route(sa::AbstractSimulatedAnnealing)::Tuple{Elements.Route, Float64}
+    route = Elements.Route(1:length(get_cities(sa)))
+    route_value = compute_value(sa, route)
+    return route, route_value
+end
 
-sample_from_kernel(
+function sample_from_kernel(
     sa::AbstractSimulatedAnnealing,
     route::Elements.Route,
-)::Elements.Route = sample(get_kernel(sa), route)
+)::Tuple{Elements.Route, Float64}
+    new_route = get_kernel(sa)(route)
+    dist_new_route = compute_value(sa, new_route)
+    return new_route, dist_new_route
+end
 
-compute_value(sa::AbstractSimulatedAnnealing, route::Elements.Route)::Float64 =
-    total_distance(get_cities(sa), route)
+function compute_value(sa::AbstractSimulatedAnnealing, route::Elements.Route)::Float64
+    return total_distance(get_cities(sa), route)
+end
 
-compute_log_acceptance_prob(
+function compute_log_acceptance_prob(
     sa::AbstractSimulatedAnnealing,
     route::Elements.Route,
     new_route::Elements.Route,
     k::Int,
-)::Float64 =
-    (-compute_value(sa, new_route) + compute_value(sa, route)) /
-    (get_cooling_schedule(sa)(k) + eps(Float64))
+)::Float64
+    gap = compute_value(sa, new_route) - compute_value(sa, route)
+    if gap <= 0
+        return 0.0
+    end
+    return -gap / (get_temperature(sa)(k) + eps(Float64))
+end
 
-select_route(
-    sa::AbstractSimulatedAnnealing,
-    route::Elements.Route,
-    new_route::Elements.Route,
-    log_accept_prob::Float64,
-)::Elements.Route =
-    log_accept_prob > 0 || rand() < exp(log_accept_prob) ? new_route : route
+function stop_criteria(sa::AbstractSimulatedAnnealing, stop_counter::Int)::Bool
+    return get_early_stop(sa) && stop_counter > get_stop_after(sa)
+end
 
-stop_criteria(sa::AbstractSimulatedAnnealing, k::Int)::Bool =
-    get_early_stop(sa) && k > get_stop_after(sa)
+function run_sa(sa::AbstractSimulatedAnnealing)::Tuple{Elements.Route, Float64}
+    route, route_value = initial_route(sa)
 
-function run_sa(sa::AbstractSimulatedAnnealing)::Elements.Route
-    route = initial_route(sa)
-    route_value = compute_value(sa, route)
     best_route = route
     best_value = route_value
+
+    stop_counter = 0
     for k ∈ 1:get_n_iter(sa)
-        # route = step_sa(sa, route, k)
-        new_route = sample_from_kernel(sa, route)
+        new_route, new_route_value = sample_from_kernel(sa, route)
+
         log_accept_prob = compute_log_acceptance_prob(sa, route, new_route, k)
-        route = select_route(sa, route, new_route, log_accept_prob)
-        route_value = compute_value(sa, route)
-        if route_value < best_value
-            best_route = route
-            best_value = route_value
+
+        if log_accept_prob > 0 || rand() < exp(log_accept_prob)
+            stop_counter = 0
+
+            if new_route_value < best_value
+                best_route = new_route
+                best_value = new_route_value
+            end
+
+            route = new_route
+            route_value = new_route_value
+
+            callback_after_acceptance(sa, k)
+        else
+            stop_counter += 1
         end
-        if stop_criteria(sa, k)
+
+        callback_after_iteration(sa, k)
+
+        if stop_criteria(sa, stop_counter)
             break
         end
     end
-    return best_route
+    return best_route, best_value
 end
